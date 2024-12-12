b789dbd33ee116b5c339a18b201fa3856d55e8e9
bata <batorgilerdenbold@gmail.com>
2024-12-12 17:40:28 +0800

    First

---
+++ index.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const userRoute = require("./routes/user");
+const authRoute = require("./routes/auth");
+const productRoute = require("./routes/product");
+
+dotenv.config();
+
+const app = express();
+
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => console.log("Connect with Mongoose"))
+  .catch((err) => {
+    console.log(err);
+  });
+
+const { ALLOWED_CORS } = process.env;
+
+var allowlist = ALLOWED_CORS?.split(",") || [];
+var corsOptionsDelegate = function (req, callback) {
+  var corsOptions;
+  if (allowlist.indexOf(req.header("Origin")) !== -1) {
+    corsOptions = { origin: true };
+  } else {
+    corsOptions = { origin: false };
+  }
+  callback(null, corsOptions);
+};
+
+app.use(cors(corsOptionsDelegate));
+app.use(express.json());
+app.use("/images", express.static("public/images"));
+app.use("/api/user", userRoute);
+app.use("/api/auth", authRoute);
+app.use("/api/product", productRoute);
+
+app.listen(process.env.PORT, () => {
+  console.log(`backend running on port: ${process.env.PORT}`);
+});
