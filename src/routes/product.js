e6acc42c85ed14742f80644c87f0acf4297053b8
bata <batorgilerdenbold@gmail.com>
2025-01-08 12:17:34 +0800

    added get by name func

--- src/routes/product.js
+++ src/routes/product.js
@@ -74,13 +74,17 @@ router.delete("/:id", verifyTokenAndAdmin, async (req, res) => {
 });
 
 //GET PRODUCT
-router.get("/find/:id", verifyTokenAndAdmin, async (req, res) => {
+router.get("/find/:name", async (req, res) => {
   try {
-    const product = await Product.findById(req.params.id);
+    const product = await Product.findOne({ name: req.params.name });
+
+    if (!product) {
+      return res.status(404).json({ message: "Product not found" });
+    }
 
     res.status(200).json(product);
   } catch (err) {
-    res.status(500).json(err);
+    res.status(500).json({ error: err.message });
   }
 });
 
